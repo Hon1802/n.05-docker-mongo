f3e3607faac83fb0f36276799952fcaf9d6d69ff
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-11-20 12:05:05 +0700

    .
    .

---
+++ sampleData/addDataTour.js
@@ -0,0 +1,28 @@
+import { handleAddNewTour } from "../src/services/toursService.js";
+import * as data from "./tour.json" assert { type: "json" };
+
+export const upload = async() => { 
+    for (var i = 0; i < data.default.length; i++)
+    {
+        try {
+
+            await uploadEach(data.default[i]);
+        }catch(e)
+        {
+            console.log('cant upload')
+        }
+       
+    }
+}
+
+async function uploadEach(item) {
+    return await handleAddNewTour(item.name, 
+        item.description, 
+        item.destination, 
+        item.region, 
+        item.duration, 
+        item.displayPrice,
+        item.childPrice, 
+        item.adultPrice, 
+        item.status)
+}
